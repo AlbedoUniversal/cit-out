d30134d64e205875d84a44baabc892bb5ae4ddd5
Sergey Bezdenezhnykh <seregabdx@gmail.com>
2023-03-04 12:55:58 +0300

    fixed error input message

--- js/sendData.js
+++ js/sendData.js
@@ -2,15 +2,23 @@ function doPost() {
   let required = document.querySelectorAll(".required");
   let error = document.querySelectorAll(".error");
   let checkbox = document.querySelector(".custom-checkbox__input");
+  let isError = false;
   required.forEach((item, index) => {
     console.log(index);
     if (item.value.length !== 0) {
       error[index].style.opacity = 0;
     } else {
       error[index].style.opacity = 1;
+      isError = true;
     }
   });
   if (!checkbox.checked) {
     document.querySelector(".checkbox-error").style.opacity = 1;
+    isError = true;
+  }
+  if (!isError) {
+    document.querySelector(".form__success").style.display = "flex";
+    document.querySelector(".form__title").style.display = "none";
+    document.querySelector(".form__content").style.display = "none";
   }
 }
